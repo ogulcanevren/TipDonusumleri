eaad8538e12d274d65c4deee1f233ec5c26996be
ogulcanevren <81776021+ogulcanevren@users.noreply.github.com>
2022-10-02 23:39:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte a = 5;
+        short b = 20;
+        sbyte c = 25;
+
+        int d = a + b + c;
+
+        Console.WriteLine("d:" + d);
+    }
+}
